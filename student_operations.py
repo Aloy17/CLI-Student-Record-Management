590b8f08b75d8abd4924b6f4153732a32fa7c1b9
Ryane Bose <ryane.bose.job@gmail.com>
2026-09-20 21:32:40 +0530

    Added exception handling and better strings for user clarity

--- student_operations.py
+++ student_operations.py
@@ -18,7 +18,8 @@ def search_student(students, field, value):
     return found_students
 
 
-def add_student(students):
+#If a duplicate ID is entered it'll still be accepted
+def add_student(students): 
 
     while True:
         student = {
@@ -26,33 +27,45 @@ def add_student(students):
                 "Name" : "",
                 "Age" : 0,
                 "Course" : ""
-                    }       
+                    } 
+        while True:     
+            try:
+                student["ID"] = int(input("Enter Student ID: "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.") 
+            
+        student["Name"] = input("Enter Student Name: ").strip().upper() 
 
-        student["ID"] = int(input("Student ID: ")) 
-        student["Name"] = input("Student Name: ").strip().upper() 
-        student["Age"] = int(input("Student Age: ")) 
-        student["Course"] = input("Student Course: ").strip().upper()
+        while True:
+            try:
+                student["Age"] = int(input("Enter Student Age: ")) 
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+            
+        student["Course"] = input("Enter Student Course: ").strip().upper()
 
-        print("Student Details Entered")
+        print("Student details entered:")
         print(student)
 
-        ansr = input("Continue? (Y/N): ").strip().upper()
+        ansr = input("Confirm adding this student? (Y/N): ").strip().upper()
 
         if ansr == "Y":
             students.append(student)
             save_student(students)
-            print("Student Details Added")
+            print("Student added successfully.")
 
-        next = input ("Add Another Student? (Y/N): ").strip().upper()
+        next = input ("Add another student? (Y/N): ").strip().upper()
         if next != "Y":
             break
     
 
-def find_student(students):
+def find_student(students): #Exception Handling for ensuring user types correct data type
     print("""
-    ========== VIEW STUDENTS ==========
+    ========== VIEW STUDENT RECORDS ==========
 
-    Choose how you want to view student records.
+    Select a search option:
 
     [1] View one student by ID
     [2] View one student by Name
@@ -60,32 +73,48 @@ def find_student(students):
     [4] View students by Course
     [5] View all students
     """)
-    choice = int(input("Type the NUMBER of your choice (1-5): "))
+
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-5): "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+            
+        if not 1 <= choice <=5:
+            print("Invalid choice. Please enter a number from 1 to 5.")
+        else:
+            break
 
     match choice:
 
         case 1:
-            student_id = int(input("Enter ID: "))
+            while True:
+                try:
+                    student_id = int(input("Enter Student ID: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
 
-            print(f"Listing Student with ID {student_id}")
+                print(f"Searching for student with ID: {student_id}")
 
-            sleep(1)
-            print(".")
-            sleep(1)
-            print(".")
+                sleep(1)
+                print(".")
+                sleep(1)
+                print(".")
 
-            found_students = search_student(students, "ID", student_id)
+                found_students = search_student(students, "ID", student_id)
 
-            if found_students:
-                for student in found_students:
-                    print(student)
-            else:
-                print("No Match Found")
+                if found_students:
+                    for student in found_students:
+                        print(student)
+                else:
+                    print("No matching student records found.")
 
 
         case 2:
-            student_name = input("Enter Name: ")
-            print(f"Listing Student with Name '{student_name}'")
+            student_name = input("Enter Student Name: ")
+            print(f"Searching for students with name: '{student_name}'")
             sleep(1)
             print(".")
             sleep(1)
@@ -97,12 +126,18 @@ def find_student(students):
                 for student in found_students:
                     print(student)
             else:
-                print("No Match Found")
+                print("No matching student records found.")
             
 
         case 3:
-            student_age = int(input("Enter Age: "))
-            print(f"Listing Students with Age '{student_age}'")
+            while True:
+                try:
+                    student_age = int(input("Enter Student Age: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+                    
+            print(f"Searching for students with age: {student_age}")
             sleep(1)
             print(".")
             sleep(1)
@@ -114,8 +149,8 @@ def find_student(students):
                 
 
         case 4:
-            student_course = input("Enter Course: ").strip().upper()
-            print(f"Listing Student with Course '{student_course}'")
+            student_course = input("Enter Student Course: ").strip().upper()
+            print(f"Searching for students enrolled in: {student_course}")
             sleep(1)
             print(".")
             sleep(1)
@@ -126,7 +161,7 @@ def find_student(students):
                     print(student)
 
         case 5:
-            print("Listing All Students")
+            print("Displaying all student records:")
             sleep(1)
             print(".")
             sleep(1)
@@ -139,35 +174,51 @@ def find_student(students):
             print("Invalid Choice")
 
 
-def update_student(students):
+def update_student(students): #Exception Handling for ensuring user types correct data type while finding and while changing
     print("""
-    ========== UPDATE STUDENT ==========
+    ========== UPDATE STUDENT RECORD ==========
 
-    Find student using:
+    Select how you want to find the student:
 
     [1] ID
     [2] Name
     """)
 
-    choice = int(input("Choose a Number (1-2): "))
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-2): "))
+            if not 1<= choice <= 2:
+                print("Invalid choice. Please enter 1 or 2.")
+            else:
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
 
     match choice:
         case 1:
-            choice = int(input("Enter ID: "))
-            found_students = search_student(students,"ID",choice)
+            while True:
+                try:
+                    choice = int(input("Enter Student ID: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
 
+            found_students = search_student(students,"ID",choice)
             if not found_students:
-                print("ID Not Found")
+                print("No student found with the specified ID.")
                 return
 
 
             
         case 2:
-            student_name = input("Enter Name: ").strip().upper()
+            student_name = input("Enter Student Name: ").strip().upper()
             found_students = search_student(students, "Name", student_name)
 
             if not found_students:
-                print("Name Not Found")
+                print("No student found with the specified name.")
                 return
 
             
@@ -185,13 +236,22 @@ def update_student(students):
         for i,student in enumerate(found_students, start=1):
             print(f"[{i}] {student}")
 
-        choice = int(input("Which Student to Update? : "))
+        while True:
+            try:
+                choice = int(input("Select the student you want to update: "))
+                if not 1<= choice <= len(found_students):
+                    print("Invalid selection. Please choose a number from the list above.")
+                else:
+                    break
+
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
 
         student = found_students[choice - 1]
 
 
 
-    print("""What would you like to edit?
+    print("""Select the field you want to update:
 
         [1] Student Name
 
@@ -200,14 +260,23 @@ def update_student(students):
         [3] Student Course
                 """)
 
-    choice = int(input("Type the NUMBER of the field you want to edit (1-3): "))
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-3): "))
+            if not 1<= choice <= 3:
+                print("Invalid choice. Please enter a number from 1 to 3.")
+            else:
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
     match choice:
         case 1:
-            ansr = input("Enter New Name: ").strip().upper()
-            print(f"Changing Name from {student["Name"]} to {ansr}")
+            ansr = input("Enter New Student Name: ").strip().upper()
+            print(f"Changing Name from {student["Name"]} to {ansr}.")
 
-            choice = input("Continue? (Y/N)").strip().upper()
+            choice = input("Confirm this change? (Y/N):").strip().upper()
             if choice == "Y":
                 student["Name"] = ansr
             else:
@@ -215,10 +284,16 @@ def update_student(students):
 
 
         case 2:
-            ansr = int(input("Enter New Age: "))
-            print(f"Changing Age from {student["Age"]} to {ansr}")
+            while True:
+                try:
+                    ansr = int(input("Enter New Student Age: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+
+            print(f"Changing Age from {student["Age"]} to {ansr}.")
         
-            choice = input("Continue? (Y/N)").strip().upper()
+            choice = input("Confirm this change? (Y/N):").strip().upper()
             if choice == "Y":
                 student["Age"] = ansr
             else:
@@ -226,10 +301,10 @@ def update_student(students):
 
 
         case 3:
-            ansr = input("Enter New Course: ").strip().upper()
-            print(f"Changing Course from {student["Course"]} to {ansr}")
+            ansr = input("Enter New Student Course: ").strip().upper()
+            print(f"Changing Course from {student["Course"]} to {ansr}.")
         
-            choice = input("Continue? (Y/N)").strip().upper()
+            choice = input("Confirm this change? (Y/N):").strip().upper()
             if choice == "Y":
                 student["Course"] = ansr
             else:
@@ -239,51 +314,81 @@ def update_student(students):
 
 def delete_student(students):
     print("""
-        ========== DELETE STUDENT ==========
+        ========== DELETE STUDENT RECORD ==========
     
-        Find student using:
+        Select how you want to find the student:
     
         [1] ID
         [2] Name
         """)
 
-    choice = int(input("Choose a Number (1-2): "))
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-2): "))
+            if not 1<= choice <= 2:
+                print("Invalid choice. Please enter 1 or 2.")
+            else:
+                break
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
     match choice:
         case 1:
-            choice = int(input("Enter ID: "))
+            while True:
+                try:
+                    choice = int(input("Enter Student ID: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+
             found_students = search_student(students,"ID",choice)
 
             if not found_students:
-                print("Match Not Found")
+                print("No matching student record found.")
             else:
                 students.remove(found_students[0])
                 save_student(students)
 
 
         case 2:
-            choice = input("Enter Name: ").strip().upper()
+            choice = input("Enter Student Name: ").strip().upper()
             found_students = search_student(students,"Name",choice)
 
             if not found_students:
-                print("Match Not Found")
+                print("No matching student record found.")
             else:
                 for i,student in enumerate(found_students, start=1):
                     print(f"[{i}] {student}")
 
-                choice = int(input("Which Student to Delete? : "))
+                while True:
+                    try:
+                        choice = int(input("Select the student you want to delete: "))
+                        if not 1<= choice <= len(found_students):
+                            print("Invalid selection. Please choose a number from the list above.")
+                        else:
+                            break
 
+                    except ValueError:
+                        print("Invalid input. Please enter a valid number.")
                 student = found_students[choice - 1]
 
-                students.remove(student)
-                save_student(students)
+            students.remove(student)
+            save_student(students)
 
 def load_student():
-    with open("students.json", "r") as f:
-        return json.load(f)
+    try:
+        with open("students.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("Student data file not found. Starting with an empty student list.")
+        return []
 
-def save_student(students):
+def save_student(students): 
     with open("students.json", "w") as f:
         json.dump(students, f, indent=4)
+        
+   
+        
 
 
